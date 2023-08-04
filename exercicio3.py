3c0f285f96625a5ddd2ffd30bc026770e9cfb02c
Enzoboccia <123950310+Enzoboccia@users.noreply.github.com>
2023-08-04 09:22:05 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,22 +1,19 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def soma_dos_aninhados(lista):
+    soma = 0
 
+    for sublist in lista:
+        for num in sublist:
+            soma += num
 
+    return soma
 
+lista = [[11, 22], [33], [44, 55, 66]]
+resultado = soma_dos_aninhados(lista)
+print(resultado)  # Output: 231
 
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
-
-
-
+outra_lista = [[1, 2, 3, 4], [3, 3], [4, 6]]
+resultado_outra_lista = soma_dos_aninhados(outra_lista)
+print(resultado_outra_lista)  # Output: 34
 
 
 

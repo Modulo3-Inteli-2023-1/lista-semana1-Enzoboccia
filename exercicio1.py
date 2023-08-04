d9449bd45245c7f469243e7a7788324745495962
Enzoboccia <123950310+Enzoboccia@users.noreply.github.com>
2023-08-04 09:15:42 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,19 +1,23 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def multiplas_operacoes(a, b):
+  
+    soma = a + b
+    
+    subtracao = a - b
+    
+    multiplicacao = a * b
+    
+    if b != 0:
+        divisao = a / b
+    else:
+        divisao = 0
+    
+    return soma, subtracao, multiplicacao, divisao
 
-
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
+resultado = multiplas_operacoes(10, 2)
+print("Soma:", resultado[0])
+print("Subtração:", resultado[1])
+print("Multiplicação:", resultado[2])
+print("Divisão:", resultado[3])
 
 
 

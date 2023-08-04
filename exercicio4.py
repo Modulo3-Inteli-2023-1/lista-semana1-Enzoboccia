d42ce0dcd02229fd6ab776f7ed90a2913b66f34c
Enzoboccia <123950310+Enzoboccia@users.noreply.github.com>
2023-08-04 09:25:39 -0300

    Update exercicio4.py

--- exercicio4.py
+++ exercicio4.py
@@ -1,21 +1,20 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def tem_duplicados(lista):
+
+    conjunto = set()
+
+    for elemento in lista:
+        if elemento in conjunto:
+            return True
+        conjunto.add(elemento)
+
+    return False
 
 
+lista1 = [1, 2, 3, 4, 5]
+print(tem_duplicados(lista1)) 
 
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
-
-
-
+lista2 = [1, 2, 3, 4, 5, 1]
+print(tem_duplicados(lista2))  
 
 
 

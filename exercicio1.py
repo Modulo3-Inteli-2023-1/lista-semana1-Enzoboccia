12f9fd55e8ecf35307489873d816ac02ae52bc56
Enzoboccia <123950310+Enzoboccia@users.noreply.github.com>
2023-08-07 19:11:38 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,9 +1,7 @@
 def multiplas_operacoes(a, b):
-  
+    # Realiza as operações
     soma = a + b
-    
     subtracao = a - b
-    
     multiplicacao = a * b
     
     if b != 0:
@@ -11,20 +9,15 @@ def multiplas_operacoes(a, b):
     else:
         divisao = 0
     
+    # Retorna as operações em uma tupla
     return soma, subtracao, multiplicacao, divisao
 
-resultado = multiplas_operacoes(10, 2)
-print("Soma:", resultado[0])
-print("Subtração:", resultado[1])
-print("Multiplicação:", resultado[2])
-print("Divisão:", resultado[3])
+# Teste da função
+valor1 = 10
+valor2 = 2
 
-
-
-
-
-
-
-
-
-
+resultados = multiplas_operacoes(valor1, valor2)
+print("Soma:", resultados[0])
+print("Subtração:", resultados[1])
+print("Multiplicação:", resultados[2])
+print("Divisão:", resultados[3])

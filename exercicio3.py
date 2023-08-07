2519183e5b5d377731f9ce6d189e0fd797b6c30a
Enzoboccia <123950310+Enzoboccia@users.noreply.github.com>
2023-08-07 19:13:28 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,24 +1,14 @@
-def soma_dos_aninhados(lista):
-    soma = 0
-
-    for sublist in lista:
-        for num in sublist:
-            soma += num
-
-    return soma
+def soma_dos_aninhados(lista_de_listas):
+    soma_total = 0
+    
+    for lista in lista_de_listas:
+        for numero in lista:
+            soma_total += numero
+    
+    return soma_total
 
 # Teste da função
-lista = [[11, 22], [33], [44, 55, 66]]
-resultado = soma_dos_aninhados(lista)
-print(resultado)  # Output: 231
-
-outra_lista = [[1, 2, 3, 4], [3, 3], [4, 6]]
-resultado_outra_lista = soma_dos_aninhados(outra_lista)
-print(resultado_outra_lista)
-
-
-
-
-
-
-
+lista_de_listas = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+resultado_soma = soma_dos_aninhados(lista_de_listas)
+print("Lista de listas:", lista_de_listas)
+print("Soma total dos elementos:", resultado_soma)
